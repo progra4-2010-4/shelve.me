fd885065da09fecafb1832f23277fe43f17f67c8
lfborjas <luis.borjas@escolarea.com>
2010-11-14 23:15:40 -0600

    pasando pruebas del primer día

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
