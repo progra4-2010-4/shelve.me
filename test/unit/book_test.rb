8e65d2a1fa8eeeca4778adb9e4b1277dc0d24213
lfborjas <luis.borjas@escolarea.com>
2010-11-15 11:51:41 -0600

    nuevas pruebas de libros!

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -5,6 +5,9 @@ class BookTest < ActiveSupport::TestCase
   test "book gets created" do 
     b = Book.new(:author=>"E.A. Poe", :title=>"The imp of the perverse", :description=>"essay on the human condition")
     assert b.save
+
+    c = Book.new(:author=>"Gaahl", :title=>"Gaahlism")
+    assert c.save
   end
 
   test "title can't be empty" do 
@@ -27,33 +30,55 @@ class BookTest < ActiveSupport::TestCase
   end
   
   test "author is capitalized" do 
-    b = Book.new(:title=>"another book", :author=>"ARTHUR C. CLARKE")
+    b = Book.new(:title=>"another book", :author=>"ARtHuR C. ClARkE")
     assert b.save
     assert_equal "Arthur C. Clarke", b.author
+  end
+
+  test "author is stored with stripped space" do 
+    b = Book.new :title=>"The Gods Themselves", :author=>"Arthur C.    Clarke   "
+    assert b.save
+    assert_equal "Arthur C. Clarke", b.author
+  end
+
+  test "title is capitalized" do 
+    b = Book.new :title=>"2001: A SpAcE OdYsSeY", :author=>"Arthur C. Clarke"
+    assert b.save
+    assert_equal "2001: A Space Odyssey", b.title
+  end
+
+  test "title is stored with extra space stripped" do
+    title = "  The     life  and  times of    James Taggart  "
+    b = Book.new(:title=>title, :author=>"Dagny Taggart")
+    
+    assert b.save
+    assert_equal "The Life And Times Of James Taggart", b.title
   end
 
   test "title and author can't be duplicated" do 
     a = books :one
     #a completely duplicated book is not allowed
+    #cf: http://siddharthdawara.blogspot.com/2008/09/rails-beforesave-and-validations.html
     b = Book.new(:title=>a.title, :author=>a.author)
-    assert !b.save
+    assert_equal false, b.save 
     assert b.errors[:title].any?
     
     #validation MUST be case insensitive
     c = Book.new(:title=>a.title.upcase, :author=>a.author)
-    assert !c.save
+    
+    assert_equal false, c.valid?
     assert c.errors[:title].any?
 
     #a book with a different author should be allowed
     d = Book.new(:title=>a.title, :author=>"Another author")
+
     assert d.save
+    
+    #slightly different books that are, nevertheless, equal, shouldn't be allowed
+    e = Book.create!(:title => "I, Robot", :author=>"Isaac Asimov")
+    f = Book.new :title=>"I,   ROBOT", :author=>"iSAaC    aSImOV"
+
+    assert_equal false, f.valid?
   end
 
-  test "title is stored with extra space stripped" do
-    title = "The     life  and  times of    James Taggart"
-    b = Book.new(:title=>title, :author=>"Dagny Taggart")
-    
-    assert b.save
-    assert_equal "The life and times of James Taggart", b.title
-  end
 end
