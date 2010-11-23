433fd4217e1056f530cb5db0e9dbab3f648e3641
lfborjas <luis.borjas@escolarea.com>
2010-11-23 17:34:00 -0600

    pruebas para el mailer

---
+++ test/functional/reviews_mailer_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ReviewsMailerTest < ActionMailer::TestCase
+  setup do 
+    @book = books :one
+    @book.readers << users(:one) << users(:two)
+    @review = reviews :one
+    @review.user = users :two
+  end    
+
+  test "An email is sent to a book subscribers" do
+    assert ReviewsMailer.respond_to? :new_review_email
+    email = ReviewsMailer.new_review_email(@book, @review).deliver
+
+    assert_equal false, ActionMailer::Base.deliveries.empty?
+    assert_equal @book.readers.collect{|r| r.email}, email.to
+    assert_equal "New review for #{@book.title}", email.subject
+    assert_match /by #{@review.user.username}/, email.encoded
+    assert_match /#{@review.review}/, email.encoded
+  end
+end
