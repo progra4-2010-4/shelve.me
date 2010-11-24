5a53514eedbe18e2fe5735ecb52f0bc74ead8110
lfborjas <luis.borjas@escolarea.com>
2010-11-24 11:27:03 -0600

    agregado paperclip al modelo

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ActiveRecord::Base
- attr_accessible :title, :author, :description
+ attr_accessible :title, :author, :description, :cover
 
  validates_presence_of :title
 
  has_many :reviews
  has_and_belongs_to_many :readers, :class_name=>"User"
+
+ has_attached_file :cover
 end
