eb634ebc3677bd48a6c49b403ac1d8393325ac4a
lfborjas <luis.borjas@escolarea.com>
2010-11-23 22:00:29 -0600

    listo el config de prueba para el mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,19 @@ ShelveMe::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.delivery_method = :file
+#  config.action_mailer.raise_delivery_errors = true 
+#  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'lfborjas.com',
+  :user_name            => 'progra4@lfborjas.com',
+  :password             => 'p4unitec',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
